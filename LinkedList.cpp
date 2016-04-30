9d7e5fad6a1cb312e42874c3713e55f0cfe54d1a
Gregory <gregoryurf@yandex.ru>
2016-04-30 22:17:58 +0300

    Done.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,30 +1,13 @@
-#include <stdlib.h>
-#include <iostream>
-using namespace std;
+#include "LinkedList.h"
 
-struct Item 
+// Constructor.
+LinkedList::LinkedList()
 {
-    int  x;    
-    Item *next, *prev;
-};
+    head_ = tail_ = NULL;
+    size_ = 0;
+}
 
-class LinkedList 
-{
-    private:
-        Item *head_,*tail_;
-        int   size_;
-    
-    public:
-        LinkedList(): head_(NULL),tail_(NULL),size_(0){};
-        ~LinkedList();
-        
-        void Print();
-        void Add(int x);
-        int  getSize();
-        
-        int operator[](int index);
-};
-
+// Destructor.
 LinkedList::~LinkedList() 
 {
     while (head_) 
@@ -36,11 +19,13 @@ LinkedList::~LinkedList()
     size_ = 0;
 }
 
+// Add int type to end of list.
 void LinkedList::Add(int x) 
 {
-    Item *temp   = new Item;
+    Item* temp   = new Item;
     temp -> next = NULL;
-    temp -> x    = x;
+    temp -> type = t_int; 
+    temp -> x    = new int(x);
     
     if (head_ != NULL) 
     {
@@ -56,51 +41,152 @@ void LinkedList::Add(int x)
     size_++;
 }
 
+// Add double type to end of list.
+void LinkedList::Add(double x) 
+{
+    Item* temp   = new Item;
+    temp -> next = NULL;
+    temp -> type = t_float; 
+    temp -> x    = new float(x);
+    
+    if (head_ != NULL) 
+    {
+        temp -> prev  = tail_;
+        tail_ -> next = temp;
+        tail_         = temp;
+    } 
+    else 
+    {
+        temp -> prev  = NULL;
+        head_ = tail_ = temp;    
+    }
+    size_++;
+}
+
+// Add string type to end of list.
+void LinkedList::Add(string x) 
+{
+    Item* temp   = new Item;
+    temp -> next = NULL;
+    temp -> type = t_string; 
+    temp -> x    = new string(x);
+    
+    if (head_ != NULL) 
+    {
+        temp -> prev  = tail_;
+        tail_ -> next = temp;
+        tail_         = temp;
+    } 
+    else 
+    {
+        temp -> prev  = NULL;
+        head_ = tail_ = temp;    
+    }
+    size_++;
+}
+
+// Get size of list.
 int LinkedList::getSize()
 {
     return size_;
 }
 
-void LinkedList::Print()
+// Remove item from list by index.
+void LinkedList::Remove(int index)
 {
-    Item *temp = head_;
-    while (temp != NULL) 
+    Item* temp = head_;
+    
+    if (index < 0 || index > (size_ - 1))
     {
-        cout << temp -> x << " ";
-        temp = temp -> next; 
+        cout << "Item with this index not founded." << endl;
     }
-    cout << endl;
+    
+    // Delete first item in list.
+    if (index == 0)
+    {
+        // If list is empty.
+        if (size_ == 0)
+        {
+            cout << "List is empty." << endl;
+            return;    
+        }
+        // If list have only 1 item.
+        else if (size_ == 1)
+        {
+            head_   = NULL;
+            tail_   = NULL;
+            size_   = 0;
+            return;
+        }
+        
+        Item* buf   = temp -> next;
+        
+        buf -> prev = NULL;
+        head_       = buf;
+        size_--;
+        return;
+    }
+    
+    // Delete last item in list.
+    if (index == size_ - 1)
+    {
+        Item* lastItem   = tail_;
+        Item* prevItem   = tail_ -> prev;
+        
+        prevItem -> next = NULL;
+        tail_            = prevItem;
+        delete lastItem;
+        size_--;
+        return;
+    }
+    
+    // Delete middle item in list.
+    int i(0);
+    while (temp -> next != NULL)
+    {
+        if (i+1 == index)
+        {
+            Item* deletedItem = temp -> next;
+            Item* nextItem    = deletedItem -> next;
+            
+            temp     -> next = nextItem;
+            nextItem -> prev = temp;
+            
+            delete deletedItem;
+            size_--;        
+            break;
+        }
+        temp = temp -> next;
+        i++;
+    }
+    
 }
 
-int LinkedList::operator[] (int index)
+// Print list.
+void LinkedList::Print()
 {
-    Item *temp = new Item;
+    Item* temp = head_;
     int i(0);
     
+    cout << "List with " << size_ << " item(s):" << endl; 
     for (temp = head_; temp != NULL; temp = temp -> next) 
     {
-        if (i == index) 
+        switch(temp->type)
         {
-            return temp->x;    
+            case t_int:
+                cout << "[" << i << " Type: int]: " << *((int*)(temp->x)) << endl;
+                break;
+                
+            case t_float:
+                cout << "[" << i << " Type: float]: " << *((float*)(temp->x)) << endl;
+                break;
+                
+            case t_string:
+                cout << "[" << i << " Type: string]: " << *((string*)(temp->x)) << endl;
+                break;
         }
         i++;
     }
-}
-
-int main (int argc, char** argv)
-{
-    LinkedList linkedList;
     
-    linkedList.Add(100); 
-    linkedList.Add(200);
-    linkedList.Add(900);
-    linkedList.Add(888);
-    
-    linkedList.Print(); 
-    for (int i=0; i < linkedList.getSize(); i++)
-    {
-        cout << i << " " << linkedList[i] << endl;
-    }
-    
-    return 0;
+    cout << endl;
 }
